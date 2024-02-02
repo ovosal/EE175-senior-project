f9481f07ced2d66205455b7ad6eaa9cfbf0bd366
Sophia W <swu241@ucr.edu>
2024-02-01 18:03:08 -0800

    alt client and server.py

--- client.py
+++ client.py
@@ -1,31 +1,53 @@
 # client.py
 
+# pickle - serialize frame to byte data
+# struct - pack each frame data
 import socket, pickle, struct
 import cv2 as cv
-import numpy
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host_ip = '169.235.86.66'
+HOST_IP = '169.235.86.66'
 PORT = 9999
-s.connect((host_ip, PORT))
-data = b""
-payload_size = struct.calcsize("Q")
-while True:
-    while len(data) < payload_size:
-        packet = s.recv(4*1024)
-        if not packet: break
-        data += packet
-    packed_msg_size = data[:payload_size]
-    data = data[payload_size:]
-    msg_size = struct.unpack("Q",packed_msg_size)[0]
 
-    while len(data) < msg_size:
-        data += s.recv(4*1024)
-    frame_data = data[:msg_size]
-    data = data[msg_size:]
-    frame = pickle.loads(frame_data)
-    cv.imshow("Received", frame)
-    key = cv.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
-s.close()
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST_IP, PORT))
+
+WIN_NAME = "CLIENT"
+
+def get_video_capture():
+    camera = cv.VideoCapture(0)
+    if camera.isOpened():
+        camera.release()
+        return cv.VideoCapture(0)
+    else:
+        return cv.VideoCapture('sample-media/sample-vid-1.mp4')
+
+
+def send_video(client_socket):
+    try:
+        vid = get_video_capture()
+        while vid.isOpened():
+            ret, frame = vid.read()
+            if not ret:
+                print("Video finished.")
+                break
+            
+            data = pickle.dumps(frame)
+            size = struct.pack("Q", len(data))
+            client_socket.sendall(size + data)
+
+            cv.imshow(WIN_NAME, frame)
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+
+    except KeyboardInterrupt:
+        print("Keyboard interrupt. Closing connection.")
+    finally:
+        client_socket.close()
+        vid.release()
+        cv.destroyAllWindows()
+
+def main():
+    send_video(client_socket)
+
+if __name__ == "__main__":
+    main()
